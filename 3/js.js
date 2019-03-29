ca72beb0f7f41eb67f6b57c13711d412a5e91c89
ali tousi <alirezastoosi@gmail.com>
2019-03-29 11:17:38 +0430

    divide operation been added

--- 3/js.js
+++ 3/js.js
@@ -4,6 +4,13 @@ var adad = document.getElementsByClassName("numbers");
 function multi(){
     var x = adad[0].value;
     var y = adad[1].value;
-    var z = x**y;
+    let z = x**y;
+    console.log(z);
+}
+
+function divide(){
+    var x = adad[0].value;
+    var y = adad[1].value;
+    let z = x/y;
     console.log(z);
 }
