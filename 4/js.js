cf4063c87bd5990012862ac1dd8952d5a8868994
ali tousi <alirezastoosi@gmail.com>
2019-03-30 18:14:52 +0430

    need more work on it, does not work yet

---
+++ 4/js.js
@@ -0,0 +1,8 @@
+function convert(){
+    var value = document.getElementById("temp").value;
+    if(isNaN(value)){
+        console.log("the temp that you've entered is not a number");
+    }else{
+        
+    }
+}
