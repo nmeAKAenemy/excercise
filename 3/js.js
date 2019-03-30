75eb9c617ef89575bd5baacce0746ae94bbff629
ali tousi <alirezastoosi@gmail.com>
2019-03-30 12:19:03 +0430

    debug for NaN values

--- 3/js.js
+++ 3/js.js
@@ -4,13 +4,23 @@ var adad = document.getElementsByClassName("numbers");
 function multi(){
     var x = adad[0].value;
     var y = adad[1].value;
-    let z = x**y;
+    if (isNaN(x))
+        var z = "'x' is not a number";
+    else if(isNaN(y))
+        var z = "'y' is not number";
+    else
+        var z = x**y;
     console.log(z);
 }
 
 function divide(){
     var x = adad[0].value;
     var y = adad[1].value;
-    let z = x/y;
+    if (isNaN(x))
+        var z = "'x' is not a number";
+    else if(isNaN(y))
+        var z = "'y' is not number";
+    else
+        var z = x/y;
     console.log(z);
 }
