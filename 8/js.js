2c03a544c40842d47373136e0f1b37492a53a368
ali tousi <alirezastoosi@gmail.com>
2019-04-04 02:07:36 +0430

    fixed the bug, now works correct

--- 8/js.js
+++ 8/js.js
@@ -2,7 +2,7 @@ function find(){
     var first = document.getElementsByClassName("value")[0].value;
     var second = document.getElementsByClassName("value")[1].value;
     var third = document.getElementsByClassName("value")[2].value;
-    var numbers = [first, second, third];
+    var numbers = [ first, second, third];
     numbers.sort(function (a, b) { return a - b });
-    console.log(numbers[0]);
+    document.getElementById("result").innerHTML = numbers[2];
 }
