c8e515a856b8497ef040771e8115a13af1610970
ali tousi <alirezastoosi@gmail.com>
2019-04-04 01:58:25 +0430

    now works better

--- 1/js.js
+++ 1/js.js
@@ -1,10 +1,13 @@
 var d = new Date();
 function main(){
     var day = d.getDay();
-    var hour = d.getHours();
+    var hours = d.getHours();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
     var min = d.getMinutes();
     var sec = d.getSeconds();
     var dow = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     document.getElementById("day").innerHTML = "Today is: " + dow[day];
-    document.getElementById("detail").innerHTML = "current time is: " + hour + ":" + min + ":" + sec ;
+    document.getElementById("detail").innerHTML = "current time is: " + hours + " " + ampm + ":" + min + ":" + sec ;
 }
