92420e7aa27c0c291eacc694b1ce8e7d31387ab4
ali tousi <alirezastoosi@gmail.com>
2019-04-04 01:03:40 +0430

    everything is right, works well

--- 14/JS.js
+++ 14/JS.js
@@ -3,16 +3,20 @@ function specify(){
     if (isNaN(angel)) {
         document.getElementById("result").innerHTML = "what you've entered is not a number, please input a number!";
     } else {
-        // var dividend = angel / 180;
-        // var real = angel - dividend * 180;
-        if (0 < angel && angel < 90) {
-            document.getElementById("result").innerHTML = "Acute angel";
-        } else if (angel == 90) {
-            document.getElementById("result").innerHTML = "Right angel";
-        } else if (90 < angel && angel < 180) {
-            document.getElementById("result").innerHTML = "Obtuse angel";
-        } else if (angel == 180) {
-            document.getElementById("result").innerHTML = "Straight angel";
+        if ( 0 < angel && angel <= 180) {
+            if (0 < angel && angel < 90) {
+                document.getElementById("result").innerHTML = "Acute angel";
+            } else if (angel == 90) {
+                document.getElementById("result").innerHTML = "Right angel";
+            } else if (90 < angel && angel < 180) {
+                document.getElementById("result").innerHTML = "Obtuse angel";
+            } else if (angel == 180) {
+                document.getElementById("result").innerHTML = "Straight angel";
+            }
+        } else if ( 180 < angel ) {
+            document.getElementById("result").innerHTML = "What you've entered is greater than 180, please input a number smaller than 180!";
+        } else {
+            document.getElementById("result").innerHTML = "What you've entered is a negative value, please turn it into positive!";
         }
     }
 }
