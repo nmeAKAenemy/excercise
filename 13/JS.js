aca3333f452ab1c297dbeb5be6b82544c8074b63
ali tousi <alirezastoosi@gmail.com>
2019-04-04 17:44:39 +0430

    files created

---
+++ 13/JS.js
@@ -0,0 +1,10 @@
+function check(){
+    var numbers = document.getElementsByClassName("numbers");
+    var number1 = numbers[0].value;
+    var number2 = numbers[1].value;
+    var number3 = numbers[2].value;
+    var number4 = numbers[3].value;
+    if () {
+        
+    }
+}
