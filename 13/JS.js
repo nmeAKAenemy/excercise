db6d898a023e629a52ce852fabd6bf3532bfe096
ali tousi <alirezastoosi@gmail.com>
2019-04-05 01:03:41 +0430

    ready to run, works well

--- 13/JS.js
+++ 13/JS.js
@@ -2,9 +2,9 @@ function check(){
     var numbers = document.getElementsByClassName("numbers");
     var number1 = numbers[0].value;
     var number2 = numbers[1].value;
-    var number3 = numbers[2].value;
-    var number4 = numbers[3].value;
-    if () {
-        
+    if ( number1 == number2 ) {
+        document.getElementById("result").innerHTML = "result: " + "Both numbers you've entered are similar to each other";
+    } else {
+        document.getElementById("result").innerHTML = "result: " + "Numbers are different";
     }
 }
