40be3b06139465639b798df569031ad7ac810cb5
ali tousi <alirezastoosi@gmail.com>
2019-04-04 15:19:57 +0430

    still is not ready

---
+++ 15/JS.js
@@ -0,0 +1,7 @@
+function compare(){
+    var inp1 = document.getElementsByClassName("arr")[0].value;
+    var inp2 = document.getElementsByClassName("arr")[0].value;
+    var arr1 = inp1.split(",");
+    var arr2 = inp2.split(",");
+    
+}
