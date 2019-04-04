5853424d9cf7926d0b119bdad30f525970a66b1f
ali tousi <alirezastoosi@gmail.com>
2019-04-04 16:44:05 +0430

    debugged

--- 6/JS.js
+++ 6/JS.js
@@ -1,18 +1,18 @@
-
-
 // function bigFunc () {
-    
-    function check() {
-    var number = document.getElementsByClassName("numbers");
-    var x = number[0];
-    var y = number[1];
-    var z = x + y;
-    if ( x == 50 || y == 50 || z == 50 ) {
-        document.getElementById("result").innerHTML = "True";
-    } else {
-        document.getElementById("result").innerHTML = "False";
+
+
+function check(){
+        var x = document.getElementsByClassName("numbers")[0];
+        var y = document.getElementsByClassName("numbers")[1];
+        // var x = number[0];
+        // var y = number[1];
+        var z = x + y;
+        if ( x = 50 || y == 50 || z == 50 ){
+            document.getElementById("result").innerHTML = "True";
+        } else {
+            document.getElementById("result").innerHTML = "False";
+        }
     }
-}
     // var value1 = Boolean ( x==50 );
     // var value2 = Boolean ( y==50 );
     // if (value1 == true || value2 == true) {
